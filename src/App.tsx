0e638152da5af3e762a5ea4c01b0596e75ab7f85
PedroFlaminio <pedroflaminio@gmail.com>
2021-07-19 20:07:13 -0300

    correcoes implementadas

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,16 @@ import "./styles/global.scss";
 function App() {
   return (
     <BrowserRouter>
-      <AuthContextProvider>
         <ThemeContextProvider>
+      <AuthContextProvider>
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/rooms/new" component={NewRoom} />
             <Route path="/rooms/:id" component={Room} />
             <Route path="/admin/rooms/:id" component={AdminRoom} />
           </Switch>
+          </AuthContextProvider>
         </ThemeContextProvider>
-      </AuthContextProvider>
     </BrowserRouter>
   );
 }
--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,57 +1,48 @@
-import { useHistory, useParams } from "react-router-dom";
+import { useHistory, useParams } from 'react-router-dom'
 
-import logoImg from "../assets/images/logo.svg";
+import logoImg from '../assets/images/logo.svg';
+import deleteImg from '../assets/images/delete.svg';
+import checkImg from '../assets/images/check.svg';
+import answerImg from '../assets/images/answer.svg';
 import logoDarkImg from "../assets/images/logo-dark.svg";
-import deleteImg from "../assets/images/delete.svg";
-import checkImg from "../assets/images/check.svg";
-import answerImg from "../assets/images/answer.svg";
 import emptyImg from "../assets/images/empty-questions.svg";
-
-import Button from "../components/Button";
-import Question from "../components/Question";
-import RoomCode from "../components/RoomCode";
 import Modal from "../components/Modal";
-// import { useAuth } from '../hooks/useAuth';
-import { useRoom } from "../hooks/useRoom";
-import { database } from "../services/firebase";
 
-import "../styles/room.scss";
-import { useState } from "react";
-import useTheme from "../hooks/useTheme";
+import Button from '../components/Button';
+import Question from '../components/Question';
+import RoomCode from '../components/RoomCode';
+// import { useAuth } from '../hooks/useAuth';
+import { useRoom } from '../hooks/useRoom';
+import { database } from '../services/firebase';
+
+import '../styles/room.scss';
+import useTheme from '../hooks/useTheme';
 import ThemeSwitch from "../components/ThemeSwitch";
+import { useState } from 'react';
 
 type RoomParams = {
   id: string;
-};
+}
 
-const AdminRoom = () => {
-  const history = useHistory();
+export default function AdminRoom() {
+  // const { user } = useAuth();
   const { theme } = useTheme();
+  const history = useHistory()
   const params = useParams<RoomParams>();
   const roomId = params.id;
 
-  const { title, questions } = useRoom(roomId);
+  const { title, questions } = useRoom(roomId)
   const [deleteMessageId, setDeleteMessageId] = useState<string | undefined>(undefined);
   const [closeRoom, setCloseRoom] = useState<boolean | undefined>(undefined);
 
-  const handleEndRoom = async () => {
+  async function handleEndRoom() {
     await database.ref(`rooms/${roomId}`).update({
       endedAt: new Date(),
-    });
-    history.push("/");
-  };
+    })
 
-  const handleCheckQuestionAsAnswered = async (questionId: string) => {
-    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
-      isAnswered: true,
-    });
-  };
+    history.push('/');
+  }
 
-  const handleHighlightQuestion = async (questionId: string) => {
-    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
-      isHighlighted: true,
-    });
-  };
   const handleDeleteQuestion = async (questionId: string) => {
     setDeleteMessageId(questionId);
   };
@@ -59,42 +50,28 @@ const AdminRoom = () => {
     await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
     setDeleteMessageId(undefined);
   };
+  // async function handleDeleteQuestion(questionId: string) {
+  //   if (window.confirm('Tem certeza que você deseja excluir esta pergunta?')) {
+  //     await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+  //   }
+  // }
+
+  async function handleCheckQuestionAsAnswered(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isAnswered: true,
+    })
+  }
+
+  async function handleHighlightQuestion(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isHighlighted: true,
+    })
+  }  
   const EmptyList = (
     <div className="empty-list">
       <img src={emptyImg} alt="Sem perguntas" />
       <h2>Nenhuma pergunta por aqui...</h2>
       <p>Faça o seu login e seja a primeira pessoa a fazer uma pergunta!</p>
-    </div>
-  );
-
-  const QuestionList = (
-    <div className="question-list">
-      {questions.map((question) => {
-        return (
-          <Question
-            key={question.id}
-            content={question.content}
-            author={question.author}
-            isAnswered={question.isAnswered}
-            isHighlighted={question.isHighlighted} 
-            dark={theme==="dark"}
-          >
-            {!question.isAnswered && (
-              <>
-                <button type="button" onClick={() => handleHighlightQuestion(question.id)}>
-                  <img src={checkImg} alt="Marcar pergunta como respondida" />
-                </button>
-                <button type="button" onClick={() => handleCheckQuestionAsAnswered(question.id)}>
-                  <img src={answerImg} alt="Dar destaque à pergunta" />
-                </button>
-              </>
-            )}
-            <button type="button" onClick={() => handleDeleteQuestion(question.id)}>
-              <img src={deleteImg} alt="Remover pergunta" />
-            </button>
-          </Question>
-        );
-      })}
     </div>
   );
 
@@ -116,10 +93,47 @@ const AdminRoom = () => {
       <main>
         <div className="room-title">
           <h1>Sala {title}</h1>
-          {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
+          { questions.length > 0 && <span>{questions.length} pergunta(s)</span> }
         </div>
-        {questions.length > 0 ? QuestionList : EmptyList}
-      </main>
+
+        <div className="question-list">
+          {questions.length > 0 ? questions.map(question => {
+            return (
+              <Question
+                key={question.id}
+                content={question.content}
+                author={question.author}
+                isAnswered={question.isAnswered}
+                isHighlighted={question.isHighlighted}
+                dark={theme==="dark"}
+              >
+                {!question.isAnswered && (
+                  <>
+                    <button
+                      type="button"
+                      onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                    >
+                      <img src={checkImg} alt="Marcar pergunta como respondida" />
+                    </button>
+                    <button
+                      type="button"
+                      onClick={() => handleHighlightQuestion(question.id)}
+                    >
+                      <img src={answerImg} alt="Dar destaque à pergunta" />
+                    </button>
+                  </>
+                )}
+                <button
+                  type="button"
+                  onClick={() => handleDeleteQuestion(question.id)}
+                >
+                  <img src={deleteImg} alt="Remover pergunta" />
+                </button>
+              </Question>
+            );
+          }): EmptyList}
+        </div>
+      </main>      
       <Modal
         title={"Excluir pergunta"}
         message={"Tem certeza que deseja excluir esta pergunta?"}
@@ -136,5 +150,4 @@ const AdminRoom = () => {
       ></Modal>
     </div>
   );
-};
-export default AdminRoom;
+}
